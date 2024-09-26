d7ff0d35b6090ed76f39234bf0717af9a3afcd54
Mohamed072005 <amine072005@gmail.com>
2024-09-26 16:24:31 +0100

    AL-7 complete the login method without the unit tests

--- routes/auth.router.js
+++ routes/auth.router.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const router = express.Router();
 const authController = require('../controllers/auth.controller');
+const { registerValidation } = require('../validations/register.input.validation')
+const { loginValidation } = require('../validations/login.input.validation')
 
-router.post('/register', authController.register);
+router.post('/register', registerValidation, authController.register);
 router.get('/users', authController.getUsers);
 router.get('/verify/account', authController.checkEmailConfirmed);
+router.post('/login', loginValidation, authController.login);
 
 module.exports = router;
